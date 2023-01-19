a365e8d6d9804e8805b6d43bfdda3096ee06027c
Nitin <nitin.bly@gmail.com>
2023-01-19 09:24:48 +0530

    Initial Project setup

---
+++ ElasticSearchSample2/src/main/java/com/elasticSearch/sample2/ElasticSearchSample2Application.java
@@ -0,0 +1,13 @@
+package com.elasticSearch.sample2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSearchSample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticSearchSample2Application.class, args);
+	}
+
+}
---
+++ ElasticSearchSample2/src/test/java/com/elasticSearch/sample2/ElasticSearchSample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elasticSearch.sample2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearchSample2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
